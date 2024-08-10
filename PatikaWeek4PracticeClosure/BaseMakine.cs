c4972520f6a73e22d320c8e26bccd371e7284e55
Bilal <bilalbilin12@gmail.com>
2024-08-10 14:31:48 +0300

    Add project files.

---
+++ PatikaWeek4PracticeClosure/BaseMakine.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4PracticeClosure
+{
+    // Creating a base class
+    public abstract class BaseMakine
+    {
+        // Defining fields 
+        private DateTime _productionDate;
+        private int _serialNumber;
+        private string _name;
+        private string _description;
+        private string _operatingSystem;
+
+        // Defining properties and encapsulation
+        public DateTime ProductionDate { get; set; }
+
+        public int SerialNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string OperatingSystem { get; set; }
+
+        // Method that display common informations
+        public virtual void BilgileriYazdir()
+        {
+            Console.WriteLine($"Üretim Tarihi --> {ProductionDate}");
+            Console.WriteLine($"Seri Numarası --> {SerialNumber}");
+            Console.WriteLine($"Adı --> {Name}");
+            Console.WriteLine($"Açıklama --> {Description}");
+            Console.WriteLine($"İşletim Sistemi --> {OperatingSystem}");
+        }
+
+        // An abstract method 
+        public abstract void UrunAdıGetir();
+     
+    }
+
+    // Creating a new class that inherits from base class
+    public class Telefon : BaseMakine
+    {
+        
+        private bool _validTrLicense; // Checking Tr licence
+
+        public bool ValidTrLicence { get; set; } 
+
+        public Telefon()
+        {
+            ProductionDate = DateTime.Now; // Whenever this constructor is run, time will be registered at that moment
+        }
+
+        public override void UrunAdıGetir() // Overriding the abstract method coming from base class
+        {
+            Console.WriteLine($"Telefonunuzun Adı --> {Name}");
+        }
+
+        public override void BilgileriYazdir() // Overriding this method for adding new properties
+        {
+            base.BilgileriYazdir();
+            Console.WriteLine(ValidTrLicence ? "TR Lisansı Var" : "TR Lisansı Yok");
+
+        }
+    }
+
+    // Creating a new class that inherits from base class
+    public class Bilgisayar : BaseMakine 
+    {
+        private int _usbPortCount; 
+
+        private bool _validBluetooth; // Checking Bluetooth feature
+
+        public int UsbPortCount 
+        {
+            get
+            {
+                return _usbPortCount;
+            }
+            set
+            {
+                if (value == 2 || value == 4) // Checking USB port number
+                {
+                    _usbPortCount = value;
+                }
+                else
+                {
+                    Console.WriteLine("USB Port sayısı sadece 2 veya 4 olabilir, başka seçenekler mevcut değildir.");
+                    _usbPortCount = -1; // If it is not 2 or 4 adding -1
+                }
+            }
+        }
+
+        public bool ValidBluetooth { get; set; }
+
+        public Bilgisayar()
+        {
+            ProductionDate = DateTime.Now; // // Whenever this constructor is run, time will be registered at that moment
+        }
+
+        public override void UrunAdıGetir() // Overriding the abstract method coming from base class
+        {
+            Console.WriteLine($"Bilgisayarınızın adı --> {Name}");
+        }
+
+        public override void BilgileriYazdir() // Overriding and adding computer properties.
+        {
+            base.BilgileriYazdir();
+            Console.WriteLine($"USB Port Sayısı --> {UsbPortCount}");
+            Console.WriteLine(ValidBluetooth ? "Bluetooth Özelliği Var" : "Bluetooth Özelliği Yok");
+        }
+    }
+}
---
+++ PatikaWeek4PracticeClosure/Program.cs
@@ -0,0 +1,122 @@
+﻿/* 
+Program akışı:
+
+1- Konsol ekranından kullanıcıya telefon üretmek için 1 bilgisayar üretmek için 2'ye basması gerektiğini söyleyiniz.
+
+2- Kullanıcının seçimine göre ilgili classtan nesne üreterek özelliklerini konsol ekranından yani kullanıcıdan alınız.
+
+3- Bütün bilgiler girildiğinde ürünün başarıyla üretildiğini bir mesaj ile bildirip başka bir ürün üretmek isteyip istemediğini sorunuz.
+
+4- Kullanıcı evet cevabını verirse 1. aşamaya geri dönünüz, hayır cevabını verirse iyi günler dileyerek uygulamayı sonlandırınız.
+*/
+
+
+using PatikaWeek4PracticeClosure;
+using System.ComponentModel.Design;
+
+string lastChoice = ""; // Defining a null string
+
+do // Using do-while loop
+{
+
+    Console.WriteLine("Merhaba At Mavalı Üretsin Yalanı Tesisine Hoşgeldiniz!");
+
+FirstQuestion: // If user enters data other than 1 or 2, it will be asked again here.
+
+    Console.Write("Telefon ve Bilgisayar arasından üretmek istediğinizi seçiniz( 1 veya 2 tuşlayın): ");
+    string firstChoice = Console.ReadLine().ToLower();
+
+    if (firstChoice == "1") // Asking phone features to the user and setting properties
+    {
+        Console.WriteLine("Telefon üretmeyi seçtiniz.");
+        Telefon telefon1 = new Telefon();
+
+        Console.Write("Lütfen telefonunuz için bir ad giriniz: ");
+        telefon1.Name = Console.ReadLine();
+
+        Console.Write("Lütfen telefonunuz için bir seri numarası giriniz: ");
+        telefon1.SerialNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Lütfen telefonunuzda olmasını istediğiniz işletim sistemini giriniz: ");
+        telefon1.OperatingSystem = Console.ReadLine();
+
+        Console.Write("Telefonunuzun TR lisansı olmasını ister misiniz? (Evet ya da Hayır olarak cevap verin lütfen): ");
+        string trLicenceChoice = Console.ReadLine().ToLower();
+
+        if (trLicenceChoice == "evet") // Checking Tr Licence
+        {
+            telefon1.ValidTrLicence = true;
+        }
+        else
+        {
+            telefon1.ValidTrLicence = false;
+        }
+
+        Console.WriteLine("Telefonunuz başarıyla oluşturuldu. İşte detayları...");
+
+        // Calling methods to display informations
+        telefon1.UrunAdıGetir();
+
+        telefon1.BilgileriYazdir();
+
+    }
+
+    else if (firstChoice == "2") // Asking computer features to the user and setting properties
+    {
+        Console.WriteLine("Bilgisayar üretmeyi seçtiniz.");
+        Bilgisayar bilgisayar1 = new Bilgisayar();
+
+        Console.Write("Lütfen bilgisayarınız için bir ad giriniz: ");
+        bilgisayar1.Name = Console.ReadLine();
+
+        Console.Write("Lütfen bilgisayarınız için bir seri numarası giriniz: ");
+        bilgisayar1.SerialNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Lütfen bilgisayarınızda olmasını istediğiniz işletim sistemini giriniz: ");
+        bilgisayar1.OperatingSystem = Console.ReadLine();
+
+        Console.Write("Lütfen bilgisayarınızda kaç adet USB Port girişi istediğinizi yazınız(2 veya 4 seçeneklerimiz mevcuttur): ");
+        bilgisayar1.UsbPortCount = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Bilgisayarınızda Bluetooth özelliği olsun ister misiniz (Evet ya da Hayır yazınız): ");
+        string bluetoothChoice = Console.ReadLine().ToLower();
+
+        if (bluetoothChoice == "evet") // Checking bluetooth features
+            bilgisayar1.ValidBluetooth = true;
+        else
+            bilgisayar1.ValidBluetooth = false;
+
+        Console.WriteLine("Bilgisayarınız başarıyla oluşturuldu. İşte detayları...");
+
+        // Calling methods to display informations
+        bilgisayar1.UrunAdıGetir();
+
+        bilgisayar1.BilgileriYazdir();
+
+    }
+
+    else // If user enters data other than 1 or 2, this condition will triggered
+    {
+        Console.WriteLine("Hatalı tuşlama yaptınız, lütfen 1 veya 2'yi tuşlayınız.");
+
+        goto FirstQuestion; 
+    }
+
+    // The user is asked whether he/she wants to create another new product again.
+    Console.WriteLine("Yeni bir ürün daha yapmak ister misiniz?");
+    lastChoice = Console.ReadLine().ToLower();
+
+    if (lastChoice == "evet")
+    {
+        Console.WriteLine("O zaman sizi başa yolluyorum\nHoopppp gittiniz.");
+    }
+    else if (lastChoice == "hayır")
+    {
+        Console.WriteLine("Peki iyi günler, ne zaman isterseniz buradayız tekrar bekleriz.");
+    }
+    else
+    {
+        Console.WriteLine("İyi günler programı sonlandırıyorum.");
+    }
+
+} while (lastChoice == "evet");
